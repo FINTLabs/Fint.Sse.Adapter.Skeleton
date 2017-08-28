41824f88ecafa90b780c52cae8c466bd99c6923f
Frode Sjovatsen <frode@fintprosjektet.no>
2017-08-28 22:16:11 +0200

    fixed logo

--- Fint.Sse.Adapter.Console/Application.cs
+++ Fint.Sse.Adapter.Console/Application.cs
@@ -79,26 +79,27 @@ namespace Fint.Sse.Adapter.Console
 			System.Console.WriteLine();
 			System.Console.WriteLine();
 			System.Console.ForegroundColor = ConsoleColor.Red;
-			System.Console.WriteLine("FFFFFFFFFFFFFFFFFFFFFFIIIIIIIIIINNNNNNNN        NNNNNNNNTTTTTTTTTTTTTTTTTTTTTTT");
-			System.Console.WriteLine("F::::::::::::::::::::FI::::::::IN:::::::N       N::::::NT:::::::::::::::::::::T");
-			System.Console.WriteLine("F::::::::::::::::::::FI::::::::IN::::::::N      N::::::NT:::::::::::::::::::::T");
-			System.Console.WriteLine("FF::::::FFFFFFFFF::::FII::::::IIN:::::::::N     N::::::NT:::::TT:::::::TT:::::T");
-			System.Console.WriteLine("  F:::::F       FFFFFF  I::::I  N::::::::::N    N::::::NTTTTTT  T:::::T  TTTTTT");
-			System.Console.WriteLine("  F:::::F               I::::I  N:::::::::::N   N::::::N        T:::::T        ");
-			System.Console.WriteLine("  F::::::FFFFFFFFFF     I::::I  N:::::::N::::N  N::::::N        T:::::T        ");
-			System.Console.WriteLine("  F:::::::::::::::F     I::::I  N::::::N N::::N N::::::N        T:::::T        ");
-			System.Console.WriteLine("  F:::::::::::::::F     I::::I  N::::::N  N::::N:::::::N        T:::::T        ");
-			System.Console.WriteLine("  F::::::FFFFFFFFFF     I::::I  N::::::N   N:::::::::::N        T:::::T        ");
-			System.Console.WriteLine("  F:::::F               I::::I  N::::::N    N::::::::::N        T:::::T        ");
-			System.Console.WriteLine("  F:::::F               I::::I  N::::::N     N:::::::::N        T:::::T        ");
-			System.Console.WriteLine("FF:::::::FF           II::::::IIN::::::N      N::::::::N      TT:::::::TT      ");
-			System.Console.WriteLine("F::::::::FF           I::::::::IN::::::N       N:::::::N      T:::::::::T      ");
-			System.Console.WriteLine("F::::::::FF           I::::::::IN::::::N        N::::::N      T:::::::::T      ");
-			System.Console.WriteLine("FFFFFFFFFFF           IIIIIIIIIINNNNNNNN         NNNNNNN      TTTTTTTTTTT      ");
-            System.Console.WriteLine();
 
-            System.Console.ForegroundColor = ConsoleColor.Green;
-			System.Console.WriteLine("   Greetings from FINTLabs!");
+			System.Console.WriteLine("        FFFFFFFFFFFFFFFFFFFFFFIIIIIIIIIINNNNNNNN        NNNNNNNNTTTTTTTTTTTTTTTTTTTTTTT");
+			System.Console.WriteLine("        F::::::::::::::::::::FI::::::::IN:::::::N       N::::::NT:::::::::::::::::::::T");
+			System.Console.WriteLine("        F::::::::::::::::::::FI::::::::IN::::::::N      N::::::NT:::::::::::::::::::::T");
+			System.Console.WriteLine("        FF::::::FFFFFFFFF::::FII::::::IIN:::::::::N     N::::::NT:::::TT:::::::TT:::::T");
+			System.Console.WriteLine("          F:::::F       FFFFFF  I::::I  N::::::::::N    N::::::NTTTTTT  T:::::T  TTTTTT");
+			System.Console.WriteLine("          F:::::F               I::::I  N:::::::::::N   N::::::N        T:::::T        ");
+			System.Console.WriteLine("          F::::::FFFFFFFFFF     I::::I  N:::::::N::::N  N::::::N        T:::::T        ");
+			System.Console.WriteLine("          F:::::::::::::::F     I::::I  N::::::N N::::N N::::::N        T:::::T        ");
+			System.Console.WriteLine("          F:::::::::::::::F     I::::I  N::::::N  N::::N:::::::N        T:::::T        ");
+			System.Console.WriteLine("          F::::::FFFFFFFFFF     I::::I  N::::::N   N:::::::::::N        T:::::T        ");
+			System.Console.WriteLine("          F:::::F               I::::I  N::::::N    N::::::::::N        T:::::T        ");
+			System.Console.WriteLine("          F:::::F               I::::I  N::::::N     N:::::::::N        T:::::T        ");
+			System.Console.WriteLine("        FF:::::::FF           II::::::IIN::::::N      N::::::::N      TT:::::::TT      ");
+			System.Console.WriteLine("        F::::::::FF           I::::::::IN::::::N       N:::::::N      T:::::::::T      ");
+			System.Console.WriteLine("        F::::::::FF           I::::::::IN::::::N        N::::::N      T:::::::::T      ");
+			System.Console.WriteLine("        FFFFFFFFFFF           IIIIIIIIIINNNNNNNN         NNNNNNN      TTTTTTTTTTT      ");
+			System.Console.WriteLine();
+
+            System.Console.ForegroundColor = ConsoleColor.Yellow;
+			System.Console.WriteLine("           Greetings from FINTLabs!");
             System.Console.ResetColor();
 
 			System.Console.WriteLine();
