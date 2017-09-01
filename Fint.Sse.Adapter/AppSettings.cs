4defd09d520ae372e544503e3f2fa9bda1f01ca7
Peter Samuel <pjsamuel3@hotmail.com>
2017-09-01 15:19:27 +0200

    Update required settings in the correct locations

--- Fint.Sse.Adapter/AppSettings.cs
+++ Fint.Sse.Adapter/AppSettings.cs
@@ -2,10 +2,8 @@
 {
     public class AppSettings
     {
-        public string SseEndpoint { get; set; }
         public string StatusEndpoint { get; set; }
         public string ResponseEndpoint { get; set; }
-        public string[] Organizations { get; set; }
         public string LogLocation { get; set; }
         }
 }
